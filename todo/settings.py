4ce235ddb68c196e4864d6ff24a6e4185d5d424a
Monika <monikagavate5@gmail.com>
2022-07-27 12:05:26 +0530

    initial commit

--- todo/settings.py
+++ todo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fxp@ye9!0*t$3_-0q3ka+#i+^aqu%#2^%3r#!u*b=k2o!x)yv5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["todoopr.herokuapp.com",'localhost']
+ALLOWED_HOSTS = ["todoapc.herokuapp.com",'localhost']
 
 
 # Application definition
