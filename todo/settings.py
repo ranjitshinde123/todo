f435fb8163d9daa5a0805fe2b892f7e9c8434376
Monika <monikagavate5@gmail.com>
2022-07-27 17:13:50 +0530

    code

--- todo/settings.py
+++ todo/settings.py
@@ -132,7 +132,7 @@ STATIC_ROOT = BASE_DIR / 'static'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage',
