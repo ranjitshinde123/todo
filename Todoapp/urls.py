43fb035860d59599ef2777d9ccb881a2075170c8
Monika <monikagavate5@gmail.com>
2022-07-27 10:33:25 +0530

    initial commit

---
+++ Todoapp/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+from django.contrib import admin
+from django.urls import path,include
+from Todoapp.views import home,login,signup,add_todo,signout,delete_todo,change_todo
+from . import views
+
+urlpatterns = [
+    path('',home,name='home'),
+    path('login/',login,name='login'),
+    path('signup/',signup),
+    path('add_todo/',add_todo),
+    path('delete_todo/<int:id>',delete_todo),
+    path('change_status/<int:id>/<str:status>',change_todo),
+    path('logout/', signout),
+ ]
