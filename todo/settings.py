fe882fc6b85d723e892414008b9103c8595cdbf7
Monika <monikagavate5@gmail.com>
2022-07-28 10:21:06 +0530

    code

--- todo/settings.py
+++ todo/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fxp@ye9!0*t$3_-0q3ka+#i+^aqu%#2^%3r#!u*b=k2o!x)yv5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["todoapc.herokuapp.com","localhost"]
 
