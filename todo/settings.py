693dd6629d0f778ca6de65a69d4dc5b4424837e4
komal <komalp.mobicloud@gmail.com>
2023-02-07 16:31:44 +0530

    commit files

--- todo/settings.py
+++ todo/settings.py
@@ -29,8 +29,8 @@ SECRET_KEY = 'django-insecure-fxp@ye9!0*t$3_-0q3ka+#i+^aqu%#2^%3r#!u*b=k2o!x)yv5
 # DEBUG = True
 #  SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
-# ALLOWED_HOSTS =['*']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS =['*']
+# ALLOWED_HOSTS = []
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 # if RENDER_EXTERNAL_HOSTNAME:    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
