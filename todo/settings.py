9f6f4c666e5c463d7ef6c3118ccb99226288f33e
komal <komalp.mobicloud@gmail.com>
2023-02-08 10:24:53 +0530

    commit setting

--- todo/settings.py
+++ todo/settings.py
@@ -14,18 +14,18 @@ import os
 
 import dj_database_url
 from pathlib import Path
-from dotenv import load_dotenv
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(BASE_DIR / '.env')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SDEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
+DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
 # ALLOWED_HOSTS = []
