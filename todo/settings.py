290fa84f9458a0cfd8eb66c843b4e9f3388288eb
komal <komalp.mobicloud@gmail.com>
2023-02-08 10:34:36 +0530

    setting file

--- todo/settings.py
+++ todo/settings.py
@@ -152,7 +152,8 @@ USE_TZ = True
 # if 'RENDER' in os.environ:
 #     STATIC_URL = 'static/'
 STATIC_ROOT=BASE_DIR / 'static'
-
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'), ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
