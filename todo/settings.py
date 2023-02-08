7f44b0a6738b31d0245def539ed59ade33234098
komal <komalp.mobicloud@gmail.com>
2023-02-08 11:53:53 +0530

    last commit

--- todo/settings.py
+++ todo/settings.py
@@ -105,7 +105,8 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default='postgresql://user99:Tiger@123@localhost:5432/mydatabase', conn_max_age=600)}
+    'default': dj_database_url.config(default='postgresql://user99:Tiger@123@localhost:5432/mydatabase', conn_max_age=600)
+}
 
 # DATABASES ={
 #     "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
