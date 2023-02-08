fc9ca7abba83b0b575af291910871ac77b22b3c4
komal <komalp.mobicloud@gmail.com>
2023-02-08 17:42:15 +0530

    last c

--- todo/urls.py
+++ todo/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 
 urlpatterns = [
@@ -23,3 +25,4 @@ urlpatterns = [
     path('',include('Todoapp.urls'))
 
 ]
+urlpatterns +=staticfiles_urlpatterns()
