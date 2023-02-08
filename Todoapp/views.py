b69fc48475c1426fa07cbfb06462d80bf80dc0a5
komal <komalp.mobicloud@gmail.com>
2023-02-08 16:16:40 +0530

    last c

--- Todoapp/views.py
+++ Todoapp/views.py
@@ -1,5 +1,4 @@
-from django.contrib.auth.models import User
-from django.http import HttpResponse, HttpResponseRedirect
+
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from django.contrib.auth import authenticate, login as loginuser,logout
