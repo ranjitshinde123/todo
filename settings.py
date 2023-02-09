7a3a46d7316e9b5cdcf9dd4615b51af5faf73070
komal <komalp.mobicloud@gmail.com>
2023-02-09 10:00:07 +0530

    commit settings

--- settings.py
+++ settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'Todoapp',
 ]
-
+AUTHENTICATION_BACKENDS = (
+    ('django.contrib.auth.backends.ModelBackend'),
+)
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
